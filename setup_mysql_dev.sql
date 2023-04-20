324a76678cc9657d5601c88a6ab7fcbd1d9cf769
elcodo254 <ellawachaga@gmail.com>
2023-04-19 20:52:37 -0700

    fix comment bugs

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,11 +1,11 @@
 -- Prepares a MySQL server for the project
 -- create database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
---create new user and set password
+-- create new user and set password
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- grant user all privileges
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
---grant SELECT privilege on the database performance_schema
+-- grant SELECT privilege on the database performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
