6195953bd1f40e73d669ce46002753d0a808678e
ZwingliCaleb <calebzwingli@gmail.com>
2023-05-22 09:51:54 -0700

    new fix on the do_pack function

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,10 +2,11 @@
 
 from fabric.api import local
 from time import strftime
-from datetime import date
+
 
 def do_pack():
-    '''A script which generates a .tgz archive from the contents of the webstatic folderof airbnb clone repo'''
+    """A script that generates a .tgz archive from the contents of the web_static folder"""
+
     filename = strftime("%Y%m%d%H%M%S")
     try:
         local("mkdir -p versions")
