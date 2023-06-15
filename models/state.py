666c8c5ba8589f88b223bcfd2579631c92900f04
ZwingliCaleb <calebzwingli@gmail.com>
2023-06-15 14:33:27 -0700

    edit state.py

--- models/state.py
+++ models/state.py
@@ -1,31 +1,36 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
 from models.base_model import BaseModel, Base
-from models import storage_type
+from os import getenv
+import sqlalchemy
 from models.city import City
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'States'
-    if storage_type == 'db':
+    if models.storage_t == 'db':
+        __tablename__ = 'states'
         name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state',
-                              cascade='all, delete, delete-orphan')
+        cities = relationship('City', backref='state')
     else:
-        name = ''
+        name = ""
 
+    def __init__(self, *args, **kwargs):
+        """Initialize state"""
+        super().__init__(*args, **kwargs)
+
+    if models.storage_t != "db":
         @property
         def cities(self):
             """return the City instances list where
             state.id == current State.id
             """
-            from models import storage
-            related_cities = []
-            cities = storage.all(City)
-            for city in cities.values():
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
                 if city.state_id == self.id:
-                    related_cities.append(city)
-            return related_cities
+                    city_list.append(city)
+            return city_list
